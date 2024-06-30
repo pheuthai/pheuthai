1311756cd9ca839f85bfca02d970d2661d9d44a5
pheuthai <info.pheuthai@gmail.com>
2024-06-30 11:21:08 +0700

    Create firebase.config.js

---
+++ firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCW1fRv4FhQSXrspDRyW3LYh4ltFJ9WFmk",
+  authDomain: "otp-project-21590.firebaseapp.com",
+  projectId: "otp-project-21590",
+  storageBucket: "otp-project-21590.appspot.com",
+  messagingSenderId: "125122500344",
+  appId: "1:125122500344:web:08c6064ba18d8a0a50cd0d",
+  measurementId: "G-E2HXFSNKXR"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
